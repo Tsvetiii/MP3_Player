c22443f73109c2a6b21ebaac03d7c30b8a1f9e2a
Tsvetomila Georgieva <tsveti_ger@abv.bg>
2016-04-22 17:01:20 +0300

    Additional code to the Strings Class.

--- src/Strings.java
+++ src/Strings.java
@@ -3,4 +3,7 @@ public class Strings {
 
 	public static final String[] CATEGORIES = { "Name", "Author", "Album", "Genre" };
 	public static final String TITLE_NAME = "MP3 Player";
+	public static final String NO_INFO = "No information";
+	public static final String NOT_SELECTED = "You have not selected a song! Select one and then click the Play button.";
+	public static final String MORE_SELECTED = "You have selected more than one song! To play a song, select one and click the Play button again.";
 }
